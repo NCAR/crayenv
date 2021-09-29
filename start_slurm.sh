972ccaaa1ece1c508aa5d1dfdfc4196e0b674fa7
Sidd Ghosh <siddsghosh@gmail.com>
2021-09-29 10:42:40 -0600

    Now /tmp directed to scratch

--- start_slurm.sh
+++ start_slurm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+me=$(whoami)
 wd=$(pwd)
 source /etc/profile.d/modules.sh
 ml purge
@@ -10,9 +11,13 @@ unset -f module
 unset -f ml
 
 if [ ! -z "$PBS_JOBID" ] ; then
+    SCRATCH=/glade/scratch/${me}/.slurm/${PBS_JOBID}/var/tmp/$(hostname -s)
+    if [ ! -d ${SCRATCH} ] ; then
+        mkdir -p ${SCRATCH}
+    fi
     SCRDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     source ${SCRDIR}/slurmenv.bash
-    if [ ! -d $SLURM_PREF ] ; then
+    if [ ! -d "$SLURM_PREF/etc" ] ; then
        echo "Setting up Slurm configuration.."
        ${SCRDIR}/crconf.sh
     else
@@ -21,7 +26,7 @@ if [ ! -z "$PBS_JOBID" ] ; then
     slurmctldpid=$(ps -u ${me} -o pid,comm=|awk '$2 == "slurmctld"{print $1}')
     if [ -z "${slurmctldpid}" ] ; then
        echo "Starting slurmctld ..."
-       ${SCR_CCPREF}/ch-run -b/glade:/glade -b/tmp:/tmp --cd=${wd} \
+       ${SCR_CCPREF}/ch-run -b/glade:/glade -b${SCRATCH}:/tmp --cd=${wd} \
          --set-env=${SCR_IMAGEROOT}/ch/environment ${SCR_IMAGEROOT} -- ${SCRDIR}/slurmctld.sh
     else
        echo "found slurmctld running.."
--- start_slurmd.sh
+++ start_slurmd.sh
@@ -11,11 +11,15 @@ unset -f module
 unset -f ml
 
 if [ ! -z "$PBS_JOBID" ] ; then
+    SCRATCH=/glade/scratch/${me}/.slurm/${PBS_JOBID}/var/tmp/$(hostname -s)
+    if [ ! -d ${SCRATCH} ] ; then
+        mkdir -p ${SCRATCH}
+    fi
     SCRDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     slurmdpid=$(ps -u ${me} -o pid,comm=|awk '$2 == "slurmd"{print $1}')
     if [ -z "${slurmdpid}" ] ; then
        echo "Starting slurmd in node $(hostname -s)... "
-       ${SCR_CCPREF}/ch-run -b/glade:/glade -b/tmp:/tmp --cd=${wd} \
+       ${SCR_CCPREF}/ch-run -b/glade:/glade -b${SCRATCH}:/tmp --cd=${wd} \
          --set-env=${SCR_IMAGEROOT}/ch/environment ${SCR_IMAGEROOT} -- ${SCRDIR}/slurmd.sh $PBS_JOBID
     fi
 fi
